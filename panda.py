b4d087a77edcc423ebb5965cbdfb55aa7aa2cda3
alexdyaz <84355113+alexdyaz@users.noreply.github.com>
2022-02-12 20:37:17 +0000

    update 12/02

--- panda.py
+++ panda.py
@@ -1,53 +1,81 @@
-from main import *
+import base64
+import os
+import zlib
 
-import sys
 import plyer
+import sys
 
+from PyQt5.QtWidgets import QFileDialog, QApplication, QMainWindow, QPushButton
 
-
-class MyPopup(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
-
-    def paintEvent(self, e):
-        dc = QPainter(self)
-        dc.drawLine(0, 0, 100, 100)
-        dc.drawLine(100, 0, 0, 100)
+from main import *
 
 
 class Panda(Ui_mainwindow):
     def __init__(self, window):
-        self.w = MyPopup()
         self.setupUi(window)
         self.open.clicked.connect(self.abrir)
-        self.compress.clicked.connect(self.comprimir)
-        self.extract.clicked.connect(self.extrair)
+        self.compress.clicked.connect(self.encode)
+        self.extract.clicked.connect(self.decode)
         self.exit.clicked.connect(self.sair)
 
     def abrir(self):
         file = plyer.filechooser.open_file()
 
-        string = file[0]
+        return file
 
-        print(file, string)
+    def encode(self):
+        file, check = QFileDialog.getOpenFileName(None, "QFileDialog.getOpenFileName()",
+                                                  "", "All Files (*);;Python Files (*.py);;Text Files (*.txt)")
+        if check:
+            ficheiro = open(file, 'r')
+            text = ficheiro.read()
+            print(text)
 
-        return file, string
+            text1 = text.encode('ascii')
+            base64_bytes = base64.b64encode(text1)
+            encode = base64_bytes.decode('ascii')
 
-    def comprimir(self):
-        string = "lorum episodic"
-        plyer.filechooser.save_file()
+            print(encode)
 
-    def extrair(self):
-        print("butao extrair pressionado")
+            encoded, check = QFileDialog.getSaveFileName(None, "QFileDialog getSaveFileName() Demo",
+                                                         "", "All Files (*);;Python Files (*.py);;Text Files (*.txt)")
+            if check:
+                encoded = open(encoded, 'w')
+                encoded = encoded.write(encode)
+                print(encode)
+                encoded.close()
+
+            # close file
+
+    def decode(self):
+        file, check = QFileDialog.getOpenFileName(None, "QFileDialog.getOpenFileName()",
+                                                  "", "All Files (*);;Python Files (*.py);;Text Files (*.txt)")
+        if check:
+            ficheiro = open(file, 'r')
+            text = ficheiro.read()
+            print(text)
+
+            base64_bytes = text.encode('ascii')
+            message_bytes = base64.b64decode(base64_bytes)
+            decode = message_bytes.decode('ascii')
+
+            decoded, check = QFileDialog.getSaveFileName(None, "QFileDialog getSaveFileName() Demo",
+                                                     "", "All Files (*);;Python Files (*.py);;Text Files (*.txt)")
+            if check:
+                print(decode)
+                decoded = open(decoded, 'w')
+                decoded = decoded.write(decode)
+
+                decoded.close()
 
     def sair(self, ):
-        self.w.setGeometry(QRect(100, 100, 400, 200))
-        self.w.setWindowTitle('Sair')
-        self.w.show()
-        self.exit(10)
+        # self.w.setGeometry(QRect(100, 100, 400, 200))
+        # self.w.setWindowTitle('Sair')
+        # self.w.show()
+        self.exit(5)
 
-    def _file_selection_dialog(self, **kwargs):
-        raise NotImplementedError()
+    # def _file_selection_dialog(self, **kwargs):
+    #    raise NotImplementedError()
 
 
 app = QtWidgets.QApplication(sys.argv)
